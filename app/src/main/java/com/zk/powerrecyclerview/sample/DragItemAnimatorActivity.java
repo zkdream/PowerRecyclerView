c5ac8ae0c6ccd6ad51d2dd1abaad944370af77e5
zhangke <zkdreams@outlook.com>
2017-12-21 18:15:38 +0800

    第二次提交 RecyclerView的上拉加载

---
+++ app/src/main/java/com/zk/powerrecyclerview/sample/DragItemAnimatorActivity.java
@@ -0,0 +1,15 @@
+package com.zk.powerrecyclerview.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zk.powerrecyclerview.R;
+
+public class DragItemAnimatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drag_item_animator);
+    }
+}
